dfec359ae450231f2d33a4e54bdd838747ee2f90
AAROMAL-ANIL-001 <110799825+AAROMAL-ANIL-001@users.noreply.github.com>
2023-05-26 08:21:41 +0530

    first commit

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
+import { EditTodoForm } from "./EditTodoForm";
+uuidv4();
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  //
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(savedTodos);
+  }, []);
+
+  const addTodo = (todo) => {
+    const newTodos = [
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ];
+    setTodos(newTodos);
+    localStorage.setItem("todos", JSON.stringify(newTodos));
+    console.log(newTodos);
+  };
+  //
+  
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get things done !</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            toggleComplete={toggleComplete}
+            key={index}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
+    </div>
+  );
+};
